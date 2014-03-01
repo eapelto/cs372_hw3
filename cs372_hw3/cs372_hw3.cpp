b4487a24a5730beb5b4444292530d787f93a87b1
Eric Pelto <eapelto@alaska.edu>
2014-02-28 20:07:15 -0900

    Didn't like the way i was designing the project...Stared fresh. with as simple testing as possible.

--- cs372_hw3/cs372_hw3.cpp
+++ cs372_hw3/cs372_hw3.cpp
@@ -6,96 +6,46 @@
 
 #include <stdio.h>
 #include <assert.h>
+#include <crtdbg.h>
 #include "stdafx.h"
 #include <iostream>
 #include <string> 
+#include <cassert>
 using std::cout;
 using std::endl;
 using std::string;
-void onetwothree(int);
-string ConvertRomanNumerals(int);
-void CheckIfCorrect(bool);
+string RomanTester(int);
 
 
-void onetwothree(int n)
-{
-	bool working = true;
-	string correctRomanNum = "I";
-
-
-	//Loop from 1 to 3 checking if the roman numerals are correct.
-	for (int i = 1; i <= n; i++)
-	{
-		
-		if (ConvertRomanNumerals(i) == correctRomanNum)
-		{
-			cout << "Test Roman Numeral: " << i << " is " << correctRomanNum << endl;
-		}
-
-		else
-		{
-			working = false;
-			cout << "wrong romanNumeral" << endl;
-		}
-
-		correctRomanNum += "I";
-
-	}
-
-	CheckIfCorrect(working);
-
-
-
-}
-
-
-
-
-void CheckIfCorrect(bool b)
-{
-
-	//Checking if all the tests were successfull
-	if (b)
-	{
-		cout << "All tests successfull!" << endl;
-	}
-	else
-	{
-		cout << "Test not successfull" << endl;
-	}
-
-}
-
-
-
-string ConvertRomanNumerals(int n)
+string RomanTester(int n)
 {
 	string romanNum;
+
 	if (n == 1)
 	{
-		romanNum = "I";
+		romanNum == "I";
 	}
-	else if (n == 2)
-	{
-		romanNum = "II";
-	}
-	else if (n == 3) 
-	{
-		romanNum = "III";
-	}
+		
 
-
+	
 
 	return romanNum;
 
 }
 
 
+
+
+
+
 int main()
 {
 
-	onetwothree(3);
+	assert(RomanTester(1) != "I");
 
+	
+	
+	
 
 
 
